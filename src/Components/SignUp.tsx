c86cd82ca738704c6a04a1169e2d04c4029367d6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-01 01:29:58 -0400

    added basic contented to signup

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="bg-white w-full h-[100vh]">
+        <img />
+        <section>
+            <h1>Stay Updated</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+                <li>Product discovery and building what matters</li>
+                <li>Measuring to ensure updates are a success</li>
+                <li>And much more!</li>
+            </ul>
+        </section>
+        <form>
+            <label for="email">Email address</label>
+            <input id="email" type="email" placeholder="email@company.com"></input>
+            <button>Subscribe to monthly newsletter</button>
+        </form>
+    </div>
   )
 }
 
